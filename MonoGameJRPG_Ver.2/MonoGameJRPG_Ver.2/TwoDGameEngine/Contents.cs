6171e68df70e7f87c793b8602951c9ba34980d3e
Unknown <moritz97.s@web.de>
2018-08-16 19:22:11 +0200

    Contents class wraps loading of Content and makes it globally available.

---
+++ MonoGameJRPG_Ver.2/MonoGameJRPG_Ver.2/TwoDGameEngine/Contents.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGameJRPG_Ver._2.TwoDGameEngine
+{
+    /// <summary>
+    /// Holds Textures and SpriteFonts for loading and usage.
+    /// </summary>
+    public static class Contents
+    {
+        #region Texture2Ds
+    
+        #region BackgroundImages
+
+        public static Texture2D space;
+        public static Texture2D samurai;
+        public static Texture2D blueBackground;
+
+        #endregion
+        #region Menu
+
+        public static Texture2D btnNoHover;
+        public static Texture2D btnHover;
+
+        #endregion
+        #region Characters
+
+        public static Texture2D warrior;
+
+        #endregion
+
+        #endregion
+
+        #region SpriteFonts
+
+        public static SpriteFont fontNoHover;
+        public static SpriteFont fontHover;
+
+        #endregion
+
+        /// <summary>
+        /// Uses given ContentManager to load all Content declared inside Contents class.
+        /// </summary>
+        /// <param name="c"></param>
+        public static void LoadAll(ContentManager c)
+        {
+
+        }
+
+
+    }
+}
