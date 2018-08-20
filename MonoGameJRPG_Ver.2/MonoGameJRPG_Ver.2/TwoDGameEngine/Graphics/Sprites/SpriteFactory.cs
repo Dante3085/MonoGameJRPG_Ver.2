49e681585cf85c7ed3003d0de182ee576d253f0b
Unknown <moritz97.s@web.de>
2018-08-21 01:04:46 +0200

    Idle Animations for Swordsman; Unique Fps for each Animation
    Unique Fps for Idle Animation does not work correctly. 
    Fps passed to AnimatedSprite is used, not Fps for EAnimation.Idle

--- MonoGameJRPG_Ver.2/MonoGameJRPG_Ver.2/TwoDGameEngine/Graphics/Sprites/SpriteFactory.cs
+++ MonoGameJRPG_Ver.2/MonoGameJRPG_Ver.2/TwoDGameEngine/Graphics/Sprites/SpriteFactory.cs
@@ -12,42 +12,8 @@ namespace MonoGameJRPG_Ver._2.TwoDGameEngine.Graphics.Sprites
 {
     public static class SpriteFactory
     {
-        #region MenuElements
+        #region Characters
 
-        public static AnimatedSprite NewGameButton(Vector2 position)
-        {
-            Texture2D btnTexture = Contents.btnNewGame;
-            if (btnTexture == null)
-                throw new Exception("Contents.btnNewGame Texture not loaded!");
-
-            AnimatedSprite newGameBtn = new AnimatedSprite("NewGameBtn", Contents.btnNewGame, position, 5);
-            newGameBtn.AddAnimation(EAnimation.Idle, 12, 48, 13, 0, 0, Vector2.Zero);
-            return newGameBtn;
-        }
-        public static AnimatedSprite GlowingButton(Vector2 position)
-        {
-            Texture2D glowingButtonTex = Contents.glowingButton;
-            if (glowingButtonTex == null)
-                throw new Exception("Contents.glowingButton Texture not loaded!");
-
-            AnimatedSprite glowingButton = new AnimatedSprite("GlowingButton", glowingButtonTex, position, 12);
-            glowingButton.AddAnimation(EAnimation.Idle, 1, 120, 50, 0, 0, Vector2.Zero);
-            glowingButton.AddAnimation(EAnimation.MouseHover, 14, 120, 50, 0, 0, Vector2.Zero);
-
-            return glowingButton;
-        }
-        public static AnimatedSprite DiscoButton(Vector2 position)
-        {
-            Texture2D discoButtonTex = Contents.discoButton;
-            if (discoButtonTex == null)
-                throw new Exception("Contents.DiscoButton Texture not loaded!");
-
-            AnimatedSprite discoButton = new AnimatedSprite("DiscoButton", discoButtonTex, position, 60);
-            discoButton.AddAnimation(EAnimation.Idle, 1, 18, 18, 0, 0, Vector2.Zero);
-            discoButton.AddAnimation(EAnimation.MouseHover, 36, 18, 18, 0, 0, Vector2.Zero);
-
-            return discoButton;
-        }
         public static AnimatedSprite BowlingBall(Vector2 position)
         {
             Texture2D bowlingBallTexture = Contents.bowlingBall;
@@ -61,16 +27,16 @@ namespace MonoGameJRPG_Ver._2.TwoDGameEngine.Graphics.Sprites
                 Right = Keys.D,
                 Down = Keys.S
             });
-            bowlingBall.AddAnimation(EAnimation.Idle, 1, 32, 32, 0, 0, Vector2.Zero);
-            bowlingBall.AddAnimation(EAnimation.IdleLeft, 1, 32, 32, 0, 0, Vector2.Zero);
-            bowlingBall.AddAnimation(EAnimation.IdleUp, 1, 32, 32, 0, 0, Vector2.Zero);
-            bowlingBall.AddAnimation(EAnimation.IdleRight, 1, 32, 32, 0, 0, Vector2.Zero);
-            bowlingBall.AddAnimation(EAnimation.IdleDown, 1, 32, 32, 0, 0, Vector2.Zero);
+            bowlingBall.AddAnimation(EAnimation.Idle, 1, 32, 32, 0, 0, Vector2.Zero, 1);
+            bowlingBall.AddAnimation(EAnimation.IdleLeft, 1, 32, 32, 0, 0, Vector2.Zero, 1);
+            bowlingBall.AddAnimation(EAnimation.IdleUp, 1, 32, 32, 0, 0, Vector2.Zero, 1);
+            bowlingBall.AddAnimation(EAnimation.IdleRight, 1, 32, 32, 0, 0, Vector2.Zero, 1);
+            bowlingBall.AddAnimation(EAnimation.IdleDown, 1, 32, 32, 0, 0, Vector2.Zero, 1);
 
-            bowlingBall.AddAnimation(EAnimation.Left, 9, 32, 32, 32, 0, Vector2.Zero);
-            bowlingBall.AddAnimation(EAnimation.Up, 9, 32, 32, 32, 0, Vector2.Zero);
-            bowlingBall.AddAnimation(EAnimation.Right, 9, 32, 32, 0, 0, Vector2.Zero);
-            bowlingBall.AddAnimation(EAnimation.Down, 9, 32, 32, 32, 0, Vector2.Zero);
+            bowlingBall.AddAnimation(EAnimation.Left, 9, 32, 32, 32, 0, Vector2.Zero, 10);
+            bowlingBall.AddAnimation(EAnimation.Up, 9, 32, 32, 32, 0, Vector2.Zero, 10);
+            bowlingBall.AddAnimation(EAnimation.Right, 9, 32, 32, 0, 0, Vector2.Zero, 10);
+            bowlingBall.AddAnimation(EAnimation.Down, 9, 32, 32, 32, 0, Vector2.Zero, 10);
 
             return bowlingBall;
         }
@@ -78,30 +44,76 @@ namespace MonoGameJRPG_Ver._2.TwoDGameEngine.Graphics.Sprites
         {
             Texture2D swordsmanTex = Contents.swordsman;
 
-            AnimatedSprite swordsman = new AnimatedSprite("Swordsman", swordsmanTex, position, PlayerIndex.One, 8, new KeyboardInput()
+            AnimatedSprite swordsman = new AnimatedSprite("Swordsman", swordsmanTex, position, PlayerIndex.One, 5, new KeyboardInput()
             {
                 Left = Keys.A,
                 Up = Keys.W,
                 Right = Keys.D,
                 Down = Keys.S
             });
-            swordsman.AddAnimation(EAnimation.Idle, 4, 48, 43, 465, 0, new Vector2(0, 0));
 
-            swordsman.AddAnimation(EAnimation.IdleLeft, 4, 48, 43, 465, 0, new Vector2(0, 0));
-            swordsman.AddAnimation(EAnimation.IdleUp, 4, 48, 43, 465, 0, new Vector2(0, 0));
-            swordsman.AddAnimation(EAnimation.IdleRight, 4, 48, 43, 465, 0, new Vector2(0, 0));
-            swordsman.AddAnimation(EAnimation.IdleDown, 4, 48, 43, 465, 0, new Vector2(0, 0));
+            // Idle Animation that is played on startup
+            swordsman.AddAnimation(EAnimation.Idle, 4, 48, 43, 433, 0, new Vector2(0, 0), 5);
 
-            swordsman.AddAnimation(EAnimation.Left, 8, 50, 50, 100, 0, new Vector2(0, 0));
-            swordsman.AddAnimation(EAnimation.Up, 12, 50, 50, 50, 0, new Vector2(0, 0));
-            swordsman.AddAnimation(EAnimation.Right, 8, 50, 50, 100, 8, new Vector2(0, 0));
-            swordsman.AddAnimation(EAnimation.Down, 12, 50, 50, 0, 0, new Vector2(0, 0));
+            // Idle Animations for each direction.
+            swordsman.AddAnimation(EAnimation.IdleLeft, 4, 48, 43, 528, 4, new Vector2(0, 0), 5);
+            swordsman.AddAnimation(EAnimation.IdleUp, 4, 48, 43, 528, 0, new Vector2(0, 0), 5);
+            swordsman.AddAnimation(EAnimation.IdleRight, 4, 48, 43, 480, 0, new Vector2(0, 0), 5);
+            swordsman.AddAnimation(EAnimation.IdleDown, 4, 48, 43, 433, 0, new Vector2(0, 0), 5);
+
+            // Movement Animations for each direction.
+            swordsman.AddAnimation(EAnimation.Left, 8, 50, 50, 100, 0, new Vector2(0, 0), 15);
+            swordsman.AddAnimation(EAnimation.Up, 12, 50, 50, 50, 0, new Vector2(0, 0), 15);
+            swordsman.AddAnimation(EAnimation.Right, 8, 50, 50, 100, 8, new Vector2(0, 0), 15);
+            swordsman.AddAnimation(EAnimation.Down, 12, 50, 50, 0, 0, new Vector2(0, 0), 15);
+
+            swordsman.PlayAnimation(EAnimation.Idle);
+
+            // Attack Animations for each direction.
             //swordsman.AddAnimation(9, 150, 0, "AttackDown", 70, 80, new Vector2(0, 0));
             //swordsman.AddAnimation(9, 230, 0, "AttackUp", 70, 80, new Vector2(-13, -27));
             //swordsman.AddAnimation(9, 310, 0, "AttackLeft", 70, 70, new Vector2(-30, -5));
             //swordsman.AddAnimation(9, 380, 0, "AttackRight", 70, 70, new Vector2(+15, -5));
 
             return swordsman;
+        }
+
+        #endregion
+        #region MenuElements
+
+        public static AnimatedSprite NewGameButton(Vector2 position)
+        {
+            Texture2D btnTexture = Contents.btnNewGame;
+            if (btnTexture == null)
+                throw new Exception("Contents.btnNewGame Texture not loaded!");
+
+            AnimatedSprite newGameBtn = new AnimatedSprite("NewGameBtn", Contents.btnNewGame, position, 5);
+            newGameBtn.AddAnimation(EAnimation.Idle, 12, 48, 13, 0, 0, Vector2.Zero, 10);
+            return newGameBtn;
+        }
+        public static AnimatedSprite GlowingButton(Vector2 position)
+        {
+            Texture2D glowingButtonTex = Contents.glowingButton;
+            if (glowingButtonTex == null)
+                throw new Exception("Contents.glowingButton Texture not loaded!");
+
+            AnimatedSprite glowingButton = new AnimatedSprite("GlowingButton", glowingButtonTex, position, 12);
+            glowingButton.AddAnimation(EAnimation.Idle, 1, 120, 50, 0, 0, Vector2.Zero, 1);
+            glowingButton.AddAnimation(EAnimation.MouseHover, 14, 120, 50, 0, 0, Vector2.Zero, 10);
+
+            return glowingButton;
+        }
+        public static AnimatedSprite DiscoButton(Vector2 position)
+        {
+            Texture2D discoButtonTex = Contents.discoButton;
+            if (discoButtonTex == null)
+                throw new Exception("Contents.DiscoButton Texture not loaded!");
+
+            AnimatedSprite discoButton = new AnimatedSprite("DiscoButton", discoButtonTex, position, 60);
+            discoButton.AddAnimation(EAnimation.Idle, 1, 18, 18, 0, 0, Vector2.Zero, 1);
+            discoButton.AddAnimation(EAnimation.MouseHover, 36, 18, 18, 0, 0, Vector2.Zero, 10);
+
+            return discoButton;
         }
 
         #endregion
